51a712762047316e1d267ed4e1e4c4c2d1101386
severak <strajt@dalten.cz>
2024-09-11 20:25:20 +0200

    Better error messages if possible.

--- js/bluescreen.js
+++ js/bluescreen.js
@@ -1,7 +1,16 @@
 // Windows 98 like bluescreen
 window.onerror = function(message, source, lineno, colno, error) {
+    var errorMsg = '';
+    if (error && error.stack) {
+        errorMsg = error.stack
+    } else {
+        if (source && lineno) {
+            errorMsg = source + ':' + lineno  + ':\n';
+        }
+        errorMsg = errorMsg + message;
+    }
     var pre = document.createElement('pre');
-    pre.innerText = source + ':' + lineno  + ':\n' + message;
+    pre.innerText =  errorMsg;
     pre.style.fontFamily = 'monospace';
     pre.style.backgroundColor = 'blue';
     pre.style.color = 'white';
